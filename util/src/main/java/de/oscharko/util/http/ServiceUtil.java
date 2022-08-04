ca6b4359f9dabb8561ff88b6fd4ffc8a099f35ab
oscharko <o.scharkowski@oscharko.de>
2022-08-04 10:08:36 +0200

    Adding utility class ServiceUtil #14

---
+++ util/src/main/java/de/oscharko/util/http/ServiceUtil.java
@@ -0,0 +1,56 @@
+package de.oscharko.util.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * Created by oscharko on 04.08.22 - 10:04 😎
+ * Check out -> www.oscharko.de
+ * Microservices-with-Spring-Boot-and-Spring-Cloud
+ * Inside the module - util-http
+ * Inside the package - de.oscharko.util.http
+ */
+@Component
+public class ServiceUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceUtil.class);
+
+    private final String port;
+
+    private String serviceAddress = null;
+
+    @Autowired
+    public ServiceUtil(@Value("${server.port}") String port) {
+
+        this.port = port;
+    }
+
+    public String getServiceAddress() {
+        if (serviceAddress == null) {
+            serviceAddress = findMyHostname() + "/" + findMyIpAddress() + ":" + port;
+        }
+        return serviceAddress;
+    }
+
+    private String findMyHostname() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "unknown host name";
+        }
+    }
+
+    private String findMyIpAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "unknown IP address";
+        }
+    }
+}
