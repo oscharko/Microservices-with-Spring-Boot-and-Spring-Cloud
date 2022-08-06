de82cb1142f96d6e244a3cd68e532740ce03cc19
oscharko <o.scharkowski@oscharko.de>
2022-08-06 11:33:14 +0200

    Adding annotation @ComponentScan() to the microservices product-composite-service, recommendation-service and review-service closes #34

--- product-composite-service/src/main/java/de/oscharko/microservices/composite/product/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/de/oscharko/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -3,9 +3,11 @@ package de.oscharko.microservices.composite.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan("de.oscharko")
 public class ProductCompositeServiceApplication {
 
 	@Bean
