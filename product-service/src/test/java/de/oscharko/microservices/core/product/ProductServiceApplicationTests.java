c2a4ce21f9a87d173a3a247f3bbef764e5ed127e
oscharko <o.scharkowski@oscharko.de>
2022-08-07 11:25:05 +0200

    Rework the class ProductServiceApplicationTests closes #44

--- product-service/src/test/java/de/oscharko/microservices/core/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/de/oscharko/microservices/core/product/ProductServiceApplicationTests.java
@@ -5,13 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ProductServiceApplicationTests {
 
 	private static final int PRODUCT_ID_OK = 1;
 	private static final int PRODUCT_ID_NOT_FOUND = 13;
+	private static final int PRODUCT_ID_INVALID = -1;
 	@Autowired
 	private WebTestClient client;
 
@@ -28,7 +32,19 @@ class ProductServiceApplicationTests {
 				.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK);
 	}
 
+	@Test
+	void getProductInvalidParameterString() {
 
+		client.get()
+				.uri("/product/no-integer")
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(BAD_REQUEST)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/product/no-integer")
+				.jsonPath("$.message").isEqualTo("Type mismatch.");
+	}
 
 	@Test
 	void getProductNotFound() {
@@ -42,5 +58,19 @@ class ProductServiceApplicationTests {
 				.expectBody()
 				.jsonPath("$.path").isEqualTo("/product/" + PRODUCT_ID_NOT_FOUND)
 				.jsonPath("$.message").isEqualTo("No product found for productId: " + PRODUCT_ID_NOT_FOUND);
+	}
+
+	@Test
+	void getProductInvalidParameterNegativeValue() {
+
+		client.get()
+				.uri("/product/" + PRODUCT_ID_INVALID)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/product/" + PRODUCT_ID_INVALID)
+				.jsonPath("$.message").isEqualTo("Invalid productId: " + PRODUCT_ID_INVALID);
 	}
 }
