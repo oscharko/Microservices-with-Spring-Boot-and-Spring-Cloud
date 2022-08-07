8fcd8ca274543ff717717b796a8345e81907efae
oscharko <o.scharkowski@oscharko.de>
2022-08-07 10:36:39 +0200

    Adding test method getProductNotFound in microservice product-service closes #42

--- product-service/src/test/java/de/oscharko/microservices/core/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/de/oscharko/microservices/core/product/ProductServiceApplicationTests.java
@@ -10,21 +10,37 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	private static final int PRODUCT_ID_OK = 1;
+	private static final int PRODUCT_ID_NOT_FOUND = 13;
 	@Autowired
 	private WebTestClient client;
 
 	@Test
 	void getProductById() {
 
-		int productId = 1;
-
 		client.get()
-				.uri("/product/" + productId)
+				.uri("/product/" + PRODUCT_ID_OK)
 				.accept(APPLICATION_JSON)
 				.exchange()
 				.expectStatus().isOk()
 				.expectHeader().contentType(APPLICATION_JSON)
 				.expectBody()
-				.jsonPath("$.productId").isEqualTo(productId);
+				.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK);
+	}
+
+
+
+	@Test
+	void getProductNotFound() {
+
+		client.get()
+				.uri("/product/" + PRODUCT_ID_NOT_FOUND)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/product/" + PRODUCT_ID_NOT_FOUND)
+				.jsonPath("$.message").isEqualTo("No product found for productId: " + PRODUCT_ID_NOT_FOUND);
 	}
 }
