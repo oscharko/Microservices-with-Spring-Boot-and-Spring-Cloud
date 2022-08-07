bc8111a1bb06fa90a2d7280d7edf70c5df38998b
oscharko <o.scharkowski@oscharko.de>
2022-08-07 12:00:34 +0200

    Adding the test methods to the class ReviewServiceApplicationTests closes #46

--- review-service/src/test/java/de/oscharko/microservices/core/review/ReviewServiceApplicationTests.java
+++ review-service/src/test/java/de/oscharko/microservices/core/review/ReviewServiceApplicationTests.java
@@ -1,13 +1,91 @@
 package de.oscharko.microservices.core.review;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ReviewServiceApplicationTests {
 
+	private static final int PRODUCT_ID_OK = 1;
+	private static final int PRODUCT_ID_NOT_FOUND = 213;
+	private static final int PRODUCT_ID_INVALID = -1;
+
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void getReviewsByProductId() {
+
+		client.get()
+				.uri("/review?productId=" + PRODUCT_ID_OK)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(3)
+				.jsonPath("$[0].productId").isEqualTo(PRODUCT_ID_OK);
 	}
 
+	@Test
+	void getReviewsMissingParameter() {
+
+		client.get()
+				.uri("/review")
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(BAD_REQUEST)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/review")
+				.jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present");
+	}
+
+	@Test
+	void getReviewsInvalidParameter() {
+
+		client.get()
+				.uri("/review?productId=no-integer")
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(BAD_REQUEST)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/review")
+				.jsonPath("$.message").isEqualTo("Type mismatch.");
+	}
+
+	@Test
+	void getReviewsNotFound() {
+
+		client.get()
+				.uri("/review?productId=" + PRODUCT_ID_NOT_FOUND)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(0);
+	}
+
+	@Test
+	void getReviewsInvalidParameterNegativeValue() {
+
+		client.get()
+				.uri("/review?productId=" + PRODUCT_ID_INVALID)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/review")
+				.jsonPath("$.message").isEqualTo("Invalid productId: " + PRODUCT_ID_INVALID);
+	}
 }
