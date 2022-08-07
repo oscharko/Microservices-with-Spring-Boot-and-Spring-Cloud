3183c000730304c04bffec6bdc28054651eb4346
oscharko <o.scharkowski@oscharko.de>
2022-08-07 09:08:46 +0200

    Implement microservice review-service with class ReviewServiceImpl closes #40

---
+++ review-service/src/main/java/de/oscharko/microservices/core/review/services/ReviewServiceImpl.java
@@ -0,0 +1,55 @@
+package de.oscharko.microservices.core.review.services;
+
+import de.oscharko.api.core.review.Review;
+import de.oscharko.api.core.review.ReviewService;
+import de.oscharko.api.exceptions.InvalidInputException;
+import de.oscharko.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * IntelliJ IDEA 2022.2 (Ultimate Edition)
+ * Created by oscharko on 07.08.22 - 09:06 😎
+ * Check out -> www.oscharko.de
+ * Microservices-with-Spring-Boot-and-Spring-Cloud
+ * Inside the package - de.oscharko.microservices.core.review.services
+ */
+@RestController
+public class ReviewServiceImpl implements ReviewService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ReviewServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public List<Review> getReviews(int productId) {
+
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+
+        if (productId == 213) {
+            LOG.debug("No reviews found for productId: {}", productId);
+            return new ArrayList<>();
+        }
+
+        List<Review> list = new ArrayList<>();
+        list.add(new Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.getServiceAddress()));
+        list.add(new Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.getServiceAddress()));
+        list.add(new Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.getServiceAddress()));
+
+        LOG.debug("/reviews response size: {}", list.size());
+
+        return list;
+    }
+}
