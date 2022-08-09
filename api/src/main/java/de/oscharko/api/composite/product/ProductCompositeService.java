961216b5f47e5de19c0bd9a06949f736fce84dd9
oscharko <o.scharkowski@oscharko.de>
2022-08-09 19:53:51 +0200

    Adding the swagger description to interface ProductCompositeService in the api library closes #53

--- api/src/main/java/de/oscharko/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/de/oscharko/api/composite/product/ProductCompositeService.java
@@ -1,5 +1,9 @@
 package de.oscharko.api.composite.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,7 +15,18 @@ import org.springframework.web.bind.annotation.PathVariable;
  * Inside the module - api
  * Inside the package - de.oscharko.api.composite.product
  */
+@Tag(name = "ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
+
+    @Operation(
+            summary = "${api.product-composite.get-composite-product.description}",
+            description = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
 
     @GetMapping(
             value = "/product-composite/{productId}",
