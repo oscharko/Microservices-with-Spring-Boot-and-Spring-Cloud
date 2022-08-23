7123c42983c11e67b5bf6db9879bd39651d9c8e2
Oliver Scharkowski <o.scharkowski@oscharko.de>
2022-08-23 10:38:38 +0200

    Adding content variable to review class closes #65

--- api/src/main/java/de/oscharko/api/composite/product/ReviewSummary.java
+++ api/src/main/java/de/oscharko/api/composite/product/ReviewSummary.java
@@ -12,6 +12,14 @@ public class ReviewSummary {
     private final int reviewId;
     private final String author;
     private final String subject;
+    private final String content;
+
+    public ReviewSummary() {
+        this.reviewId = 0;
+        this.author = null;
+        this.subject = null;
+        this.content = null;
+    }
 
     public ReviewSummary(int reviewId, String author, String subject) {
         this.reviewId = reviewId;
@@ -29,5 +37,9 @@ public class ReviewSummary {
 
     public String getSubject() {
         return subject;
+    }
+
+    public String getContent() {
+        return content;
     }
 }
