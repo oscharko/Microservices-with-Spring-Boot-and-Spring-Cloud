5f9d6b6691537e65e02defff58092e37150d943a
Oliver Scharkowski <o.scharkowski@oscharko.de>
2022-08-23 14:18:01 +0200

    Change the argument status in the recommendation class from constant to variable and adding setters for all arguments closes #69

--- api/src/main/java/de/oscharko/api/core/recommendation/Recommendation.java
+++ api/src/main/java/de/oscharko/api/core/recommendation/Recommendation.java
@@ -9,12 +9,12 @@ package de.oscharko.api.core.recommendation;
  * Inside the package - de.oscharko.api.core.recommendation
  */
 public class Recommendation {
-    private final int productId;
-    private final int recommendationId;
-    private final String author;
-    private final int rate;
-    private final String content;
-    private final String serviceAddress;
+    private int productId;
+    private int recommendationId;
+    private String author;
+    private int rate;
+    private String content;
+    private String serviceAddress;
 
     public Recommendation() {
         productId = 0;
@@ -56,5 +56,29 @@ public class Recommendation {
 
     public String getServiceAddress() {
         return serviceAddress;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setRecommendationId(int recommendationId) {
+        this.recommendationId = recommendationId;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setServiceAddress(String serviceAddress) {
+        this.serviceAddress = serviceAddress;
     }
 }
