e6013cb83e5928f27171321c550a96cfca39b563
Oliver Scharkowski <o.scharkowski@oscharko.de>
2022-08-23 14:47:43 +0200

    Adding post and delete mapping to review service closes #71

--- api/src/main/java/de/oscharko/api/core/review/ReviewService.java
+++ api/src/main/java/de/oscharko/api/core/review/ReviewService.java
@@ -1,7 +1,6 @@
 package de.oscharko.api.core.review;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +15,36 @@ import java.util.List;
 public interface ReviewService {
 
     /**
+     * Sample usage, see below.
+     * curl -X POST $HOST:$PORT/review \
+     * -H "Content-Type: application/json" --data \
+     * '{"productId":123,"reviewId":456,"author":"me","subject":"Lorem ipsum dolor sit amet, cons","content":"Lorem ipsum dolor sit amet"}'
+     *
+     * @param body A JSON representation of the new review
+     * @return A JSON representation of the newly created review
+     */
+    @PostMapping(
+            value = "/review",
+            consumes = "application/json",
+            produces = "application/json")
+    Review createReview(@RequestBody Review body);
+
+    /**
      * Sample usage: "curl $HOST:$PORT/review?productId=1".
      *
-     * @param productId Id of the product
+     * @param productId ID of the product
      * @return the reviews of the product
      */
     @GetMapping(
             value = "/review",
             produces = "application/json")
     List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+
+    /**
+     * Sample usage: "curl -X DELETE $HOST:$PORT/review?productId=1".
+     *
+     * @param productId ID of the product
+     */
+    @DeleteMapping(value = "/review")
+    void deleteReviews(@RequestParam(value = "productId", required = true) int productId);
 }
